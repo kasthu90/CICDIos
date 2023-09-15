6c9f845148097c4440c68f1c170b7c871d9d08fb
Kasthuri <tkasthu.90@gmail.com>
2023-09-15 10:33:22 +1000

    Initial Commit

---
+++ RetirementCalculator/RetirementCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetirementCalculatorApp.swift
+//  RetirementCalculator
+//
+//  Created by Kasthuri Tandavarayane on 15/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct RetirementCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
